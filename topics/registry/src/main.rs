ec131dbc34209c6c0fe052c3ea05537c19746e27
Robin Herrmann <Robin.Herrmann@dormakaba-external.com>
2022-12-29 19:19:39 +0100

    compile again

--- topics/registry/src/main.rs
+++ topics/registry/src/main.rs
@@ -1,4 +1,4 @@
-use aws_sdk_dynamodb::{model::AttributeValue, output::GetItemOutput};s
+use aws_sdk_dynamodb::{model::AttributeValue, output::GetItemOutput};
 pub use lambda_http::aws_lambda_events::{serde::Deserialize, serde_json};
 use lambda_runtime::{service_fn, Error, LambdaEvent};
 use protocol::{message_request_registartion::RequestRegistrationDescirption, message_set_name};
