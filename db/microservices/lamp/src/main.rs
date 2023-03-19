db3c7b08118b759d47e8a4a98fecb033dcd09989
Robin Herrmann <Robin.Herrmann@dormakaba-external.com>
2023-03-19 22:22:05 +0100

    control led state via api gateway

--- db/microservices/lamp/src/main.rs
+++ db/microservices/lamp/src/main.rs
@@ -1,6 +1,8 @@
 pub use lambda_http::aws_lambda_events::{serde::Deserialize, serde_json};
 
+use aws_lambda_events::{self, apigw::ApiGatewayV2httpRequest};
 use lambda_runtime::{service_fn, Error, LambdaEvent};
+use protocol::message_lamp_state;
 use serde_json::Value;
 
 #[tokio::main]
@@ -10,19 +12,31 @@ async fn main() -> Result<(), Error> {
     Ok(())
 }
 
-async fn lamp(sth: LambdaEvent<Value>) -> Result<(), Error> {
+async fn lamp(event: LambdaEvent<Value>) -> Result<(), Error> {
+    let http_request: ApiGatewayV2httpRequest =
+        serde_json::from_str(&event.payload.to_string()).unwrap();
     println!("Hello World from Lamp!");
     let config = aws_config::load_from_env().await;
     let iotdataplane = aws_sdk_iotdataplane::Client::new(&config);
-    let res = format!("{:?}", sth);
-    iotdataplane
-        .publish()
-        .topic("lamp")
-        .qos(1)
-        .payload(aws_smithy_types::Blob::new(res))
-        .send()
-        .await
-        .unwrap();
-    println!("{:?}", sth);
+    if let Some(id) = http_request.path_parameters.get_key_value("id") {
+        if let Some(state) = http_request.query_string_parameters.first("state") {
+            let payload: String;
+            if state == "true" {
+                payload = message_lamp_state::create(id.1.into(), true);
+            } else if state == "false" {
+                payload = message_lamp_state::create(id.1.into(), false);
+            } else {
+                return Ok(());
+            }
+            iotdataplane
+                .publish()
+                .topic("thing_input")
+                .qos(1)
+                .payload(aws_smithy_types::Blob::new(payload))
+                .send()
+                .await
+                .unwrap();
+        }
+    }
     Ok(())
 }
